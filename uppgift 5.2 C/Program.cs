48a3546a236fc0a34df0b6cd83a84b5d50b5f016
en301 <emilsjostrand1000@gmail.com>
2023-11-22 10:18:16 +0100

    Add project files.

---
+++ uppgift 5.2 C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift_5_2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hur många tal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            int[] tal = new int[antal];
+            Console.WriteLine("Skriv in "+ antal +" tal");
+            for (int i = 0; i < antal; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Här är talen du skrev in");
+            for (int j = 0;j < antal;j++)
+            {
+                Console.WriteLine("Tal"+ (j+1) +": "+ tal[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
